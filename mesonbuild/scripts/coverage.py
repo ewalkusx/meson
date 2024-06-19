6570400ba1c107f10b2bd4384e40545d3b52b397
Walkusz <ewelinax.walkusz@intel.com>
2024-06-19 09:39:39 +0200

    Ignore inconsistent,mismatch
    Signed-off-by: Ewelina Walkusz <ewelinax.walkusz@intel.com>

--- mesonbuild/scripts/coverage.py
+++ mesonbuild/scripts/coverage.py
@@ -143,7 +143,7 @@ def coverage(outputs: T.List[str], source_root: str, subproject_root: str, build
                                    '--remove', covinfo,
                                    *lcov_subpoject_exclude,
                                    *lcov_exe_rc_branch_coverage,
-                                   '--ignore-errors', 'unused',
+                                   '--ignore-errors', 'inconsistent,mismatch,unused',
                                    '--output-file', covinfo] + lcov_config)
             subprocess.check_call([genhtml_exe,
                                    '--prefix', build_root,
